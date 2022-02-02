9b87a586ea75b6da036b6b96f24dd098ebb93c92
31core <im31core@yandex.com>
2022-02-02 12:39:45 +0000

    Add about.html.

--- resource/env_setup.sh
+++ resource/env_setup.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/bash
 
+os_release=$(lsb_release -is)
+
 for i in [ Ubuntu Debian ]
 do
-	if [ $(lsb_release -is) = $i ]
+	if [ $os_release = $i ]
 	then
+	    sudo apt update
 		sudo apt install build-essential nasm
 	fi
 done
 
 for i in [ CentOS Fedora ]
 do
-	if [ $(lsb_release -is) = $i ]
+	if [ $os_release = $i ]
 	then
 		sudo yum install gcc nasm
 	fi
